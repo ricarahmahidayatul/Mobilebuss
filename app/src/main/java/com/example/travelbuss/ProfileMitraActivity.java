50f65d0563b1be13d1839acff9dc66cdd0e072ff
Rica Rahmahidayatul <ricarahma97@gmail.com>
2023-11-05 01:59:58 +0700

    selesai tanpa dimulai
    ada beberapa yang kurang cuman ini dulu sementara karena waktu back end memulai eksekusi harus di dampingi front end, harus singkron sih kata gua

---
+++ app/src/main/java/com/example/travelbuss/ProfileMitraActivity.java
@@ -0,0 +1,14 @@
+package com.example.travelbuss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileMitraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_mitra);
+    }
+}
---
+++ app/src/main/java/com/example/travelbuss/dataprofileActivity.java
@@ -0,0 +1,14 @@
+package com.example.travelbuss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dataprofileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dataprofile);
+    }
+}
