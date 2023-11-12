b45398b11b9061aeab565e5d2e07f24261b6592c
Rica Rahmahidayatul <ricarahma97@gmail.com>
2023-11-12 12:42:10 +0700

    RincianPembayaran

---
+++ app/src/main/java/com/example/travelbuss/rincianPembayaran.java
@@ -0,0 +1,14 @@
+package com.example.travelbuss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class rincianPembayaran extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rincian_pembayaran);
+    }
+}
